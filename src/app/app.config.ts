9caa290ecbad92be1dbfc9ab2164e86c735cc295
renanrider <renan.silva112@fatec.sp.gov.br>
2024-02-23 00:04:05 -0300

    feat: setup app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { routes } from './app.routes';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
+import { PokemonListService } from './pokemon-list/pokemon-list.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    importProvidersFrom(HttpClientModule),
+    provideHttpClient(withFetch()),
+    PokemonListService,
+  ],
 };
