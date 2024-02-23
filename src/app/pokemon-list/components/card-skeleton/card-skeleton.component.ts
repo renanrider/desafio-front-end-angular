341a9ccd31f43be76a5e94d2e40d44a11375eb7d
renanrider <renan.silva112@fatec.sp.gov.br>
2024-02-23 00:00:06 -0300

    feat: add card skeleton component

---
+++ src/app/pokemon-list/components/card-skeleton/card-skeleton.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-skeleton',
+  standalone: true,
+  templateUrl: './card-skeleton.component.html',
+  styleUrls: ['./card-skeleton.component.scss'],
+})
+export class CardSkeletonComponent {}
